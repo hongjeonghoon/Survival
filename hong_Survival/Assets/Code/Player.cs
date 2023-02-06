503448dc6a7c9ea1d931e1603322b19e4bdf6569
hongjeonghoon <me8552@naver.com>
2023-02-06 17:51:23 +0900

    Ch_move
    Char_move

---
+++ hong_Survival/Assets/Code/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;  //대각선 같은 속도
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+}
